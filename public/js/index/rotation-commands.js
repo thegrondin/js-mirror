3b6f2e2706a2eaebcf9cba4b978bea6efa2884e2
root <tomtom9461@gmail.com>
2018-03-18 20:11:56 -0400

    Replacing bad updaing css file for the index.ejs

---
+++ public/js/index/rotation-commands.js
@@ -0,0 +1,30 @@
+(function rotationCommand() {
+    
+    var commandArray = [
+        "Affiche le bulletin météo",
+        "Affiche les nouvelles locales",
+        "Affiche le compte rendu de ma nuit",
+        "Affiche le compte rendu de ma journée"
+    ];
+
+    var rotationCommandDOMElement = document.getElementById("rotation-command");
+
+    rotationCommandDOMElement.innerHTML = "Dites: " + commandArray[0];
+
+    var rotationIndex = 1;
+
+    setInterval(function () {
+
+        if (rotationIndex >= commandArray.length){
+            rotationIndex = 0;
+        }
+
+        rotationCommandDOMElement.innerHTML = "Dites: " + commandArray[rotationIndex];
+
+        rotationIndex++;
+
+        console.log(rotationIndex)
+        
+
+    }, 6000) 
+}())
