439259278ca6d88d694be0148e312a10fbeba14a
root <tomtom9461@gmail.com>
2018-03-19 00:11:14 -0400

    Sleeping time

--- public/js/index/analog-clock.js
+++ public/js/index/analog-clock.js
@@ -11,20 +11,20 @@
     var timeDOMElement = document.getElementById("time");
 
     dateDOMElement.innerHTML = getDayFormatted(new Date(), daysName, monthsName);    
-
+                           
     setInterval(function() {
         
         var date = new Date();
 
-        timeDOMElement.innerHTML = ('0'+date.getHours()).slice(-2) + ":" + ('0'+date.getMinutes()).slice(-2);
+        timeDOMElement.innerHTML = ('0'+date.getHours()).slice(-2) + ":" + ('0'+date.getMinutes()).slice(-2);                        
 
 
         if (date.getHours() == 0 && date.getMinutes() == 0 && date.getSeconds() == 0){
             dateDOMElement.innerHTML = getDayFormatted(date, daysName, monthsName);
         }
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
     }, 1000)
-
+                      
     
 
 }())
